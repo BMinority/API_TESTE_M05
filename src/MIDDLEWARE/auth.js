e372b5b7ff8a392a4df76b116bae3bf709e3c6ac
Bruno Coelho <brunocoelho66@gmail.com>
2024-07-31 14:43:14 -0300

    criacao do middleware de autenticacao

---
+++ src/MIDDLEWARE/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+//obs.: criar a variavel avaixo no .env
+const { JWT_SECRET } = process.env;
+
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) return res.status(401).json({ error: 'Token não fornecido' });
+
+    jwt.verify(token, JWT_SECRET, (err, user) => {
+        if (err) return res.status(403).json({ error: 'Token inválido' });
+        req.user = user;
+        next();
+    });
+};
+
+module.exports = authenticateToken;
