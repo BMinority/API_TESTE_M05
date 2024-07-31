e1fec1463b3de65bd3004c2cb3b715a7fa1e4c76
Bruno Coelho <brunocoelho66@gmail.com>
2024-07-31 12:08:18 -0300

    criacao do sql e configuracao do db

---
+++ src/DATABASE/pdv.sql
@@ -0,0 +1,24 @@
+create database pdv;
+
+create table usuarios (
+    id serial primary key,
+    nome varchar(100) not null,
+    email varchar(100) unique not null,
+    senha varchar(100) not null
+);
+
+create table categorias (
+    id serial primary key,
+    descricao varchar(100) not null
+);
+
+insert into categorias (descricao) values
+('Informática'), 
+('Celulares'), 
+('Beleza e Perfumaria'), 
+('Mercado'), 
+('Livros e Papelaria'), 
+('Brinquedos'), 
+('Moda'), 
+('Bebê'), 
+('Games');
