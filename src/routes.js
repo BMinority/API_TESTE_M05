233f4bfbbe0fc7204e42d9f4f58fde032b69b08b
Bruno Coelho <brunocoelho66@gmail.com>
2024-07-31 13:09:39 -0300

    criei a rota de login

--- src/routes.js
+++ src/routes.js
@@ -38,4 +38,36 @@ router.post('/usuario', async (req, res) => {
     }
 });
 
+//login
+router.post('/login', async (req, res) => {
+    const { email, senha } = req.body;
+
+    if (!email || !senha) {
+        return res.status(400).json({ error: 'Email e senha são orbigatórios.' });
+    }
+
+    try {
+        const result = await db.query('select * from usuarios where email = $1', [email]);
+        if (result.rows.length === 0) {
+            return res.status(400).json({ error: 'Email ou senha incorretos.' });
+        }
+
+        const user = result.rows[0];
+        const validPassword = await bcrypt.compare(senha, user.senha);
+
+        if (!validPassword) {
+            return res.status(400).json({ error: 'Email ou senha incorretos.' })
+        }
+
+        const token = jwt.sign({ id: user.id, nome: user.nome, email: user.email }, 'secret', {
+            expiresIn: '1h',
+        });
+
+
+        res.status(200).json({ token });
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao realizar o login.' });
+    }
+});
+
 module.exports = router;
