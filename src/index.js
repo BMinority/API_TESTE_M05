f58ccc2066ad1a40eab506ac65f7baa75ec40e5e
Bruno Coelho <brunocoelho66@gmail.com>
2024-08-02 20:20:35 -0300

    new configs

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import express from "express";
 import cors from "cors";
+import nodemailer from "nodemailer";
+
 const app = express();
 const port = process.env.PORT || 3000;
-const nodemailer = require('nodemailer');
 
 const smtp = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
     auth: {
-        user: "bugtrakers@gmail.com",
-        pass: "fxaa avww tpqd jayw"
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
     }
 });
 
@@ -35,34 +36,29 @@ new Promise((resolve, reject) => {
     });
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-
-//senha de app do gmail:
-//fxaa avww tpqd jayw
-
-//ROTAS PARA TESTE
-let users = [];
-
 app.use(express.json());
 app.use(cors());
 
+let users = [];
+
 app.get("/", (req, res) => {
-    return res.json("Hello World");
+    res.send("Hello World");
 });
 
 app.get("/users", (req, res) => {
-    return res.json(users);
-})
+    res.json(users);
+});
 
 app.post("/users", (req, res) => {
     const { name, email } = req.body;
     const newUser = {
-        id: Math.random().toString(36),
+        id: Math.random().toString(36).substr(2, 9),
         name,
         email,
     };
 
     users.push(newUser);
-    return res.json(newUser);
-})
+    res.json(newUser);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
